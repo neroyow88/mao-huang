de56a488851084db95f70e3aed68c22629de2867
neroyow88 <neroyow88@gmail.com>
2020-09-14 01:31:02 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,6 +125,7 @@ export default class Home extends React.Component<Props, State> {
           toggle={toggle}
           scale={scale}
           customPopOutData={customPopOutData}
+          showPopOut={this._showPopOut}
           hidePopOut={this._hidePopOut}
         />
       </div>
